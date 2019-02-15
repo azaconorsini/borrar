12f77f5c6c49ff3a76b7112bf521be53906da4f2
Johan J. Azacon O <j.azacon@gmail.com>
2019-02-15 17:55:34 -0400

    Adding basic code on work files, It was modified index.html, style.css and script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+    var container = document.getElementById('container');
+    var heading = document.getElementById('heading');
+    var profile = document.getElementById('profile');
+
+    function onload_site() {
+        // Add code here
+    }
+}
